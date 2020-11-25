ec2e7a783e8e59c1dc4e059ab57dd1a87736cd00
Christine-1122 <61156750+Christine-1122@users.noreply.github.com>
2020-11-25 10:46:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,95 +3,81 @@ import matplotlib.pyplot as plt
 import pickle as p
 import time
 import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
 import yfinance as yf
 import seaborn as sns
-# from yahoo_fin import stock_info
-# from plotly.offline import plot, init_notebook_mode
-# init_notebook_mode()
-# import cufflinks as cf
-# %matplotlib inline
-# cf.set_config_file(offline = True)
+import datetime
+
 
 #Using two functions here to
 #Using as a wallet to save the money
 #Another to record the usage of every payment
 
-#Here is initial value
+#The date right now
 date = time.strftime('%Y%m%d')
-#Initial value on the account this month
-# f = open('wallet.data','wb')
-# p.dump(2000,f,0)
-# with open('record.txt','wb')as f:
-# 	pass
-#Insert budget for this month 
-
-# def basic_info():
-
-# 	budget = input("Input the number of maximum spending this month: ")
-# 	total_income = input('Input total income this month: ')
-# 	balance_check = total_income - budget
 
 
 
 def save_money():
 	'''
-	Let the user save the amount of money
-	and usage to let the program know
-	and add to the previous section/balance
+	# Let the user save the amount of money
+	# and usage to let the program know
+	# and add to the previous section/balance
 	'''
+	# date = insert_date()
 	sav_amt = float(input('The amount of money you would like to save: '))
-	sav_comment = input("Doing what: ")
+	# print("Please insert the number for categories: ")
+	# print("1-Housing\t2-Food\t3-Savings\n4-Transportation\t5-Other Expenses\t6-Free Spend")
+	sav_comment =str("Savings")
 
 	f = open('wallet.data','rb')
 	balance = p.load(f)
 	f.close()
-	# with open('wallet.data') as f:
-	# 	balance = p.load(f,encoding = 'bytes')
 
 	new_balance = balance+sav_amt
 
 	f = open('wallet.data','wb')
 	p.dump(new_balance,f,0)
-	# with open('wallet.data','wb') as f:
-	# 	p.dump(new_balance,f)
+
 
 	content = '%-12s%-8s%-8s%-10s%-25s\n'%(date,'N/A',sav_amt,new_balance,sav_comment)
 	
-	with open('record.txt','a') as f:
+	with open('record.txt','w') as f:
 		#a is append here to add records; can convert to pdf statement for user
 		f.write(content)
 
 	print(content)
 		
-def spend_money(balance_check):
+def spend_money(balance_check):#Adding blance_cehck back
 	'''
 	This function tells you the money you spend
 	and the category it is used 
 	'''
+	# date = insert_date()
 	spend_amt = float(input('please insert the money you spend: '))
-	spend_comment = input("What category this for: ")
+	print("Please insert the number for categories: ")
+	print("1-Housing\t2-Food\t3-Savings\n4-Transportation\t5-Other Expenses\t6-Free Spend")
+	spend_comment_dict = {1:"Housing",2:'Food',3:'Savings',4:'Transportation',5:'Other Expenses',6:'Free Spend'}
+	s_input = int(input("What category this for: "))
+	spend_cmt = spend_comment_dict[s_input]
 
 	f = open('wallet.data','rb')
 	balance = p.load(f)
 	new_balance = balance - spend_amt
 	f.close()
-	# with open('wallet.data') as f:
-	# 	balance = p.load(f, encoding = 'bytes')
-	# new_balance = balance - spend_amt
+
 	f = open('wallet.data','wb')
 	p.dump(new_balance,f,0)
-	# with open('wallet.data','wb') as f:
-	# 	p.dump(new_balance,f)
+
 	if new_balance <= balance_check:
 			print("Attention: You spend more than your budget!!")
 
-	with open('record.txt','a') as f:
-		content = '%-12s%-8s%-8s%-10s%-25s\n'%(date, spend_amt,'N/A',new_balance,spend_comment)
+	with open('record.txt','w') as f:
+		content = '%-12s%-8s%-8s%-10s%-25s\n'%(date, spend_amt,'N/A',new_balance,spend_cmt)
 	# list.append(content)
 	print(content)
 
 def query_info():
+	# date = insert_date()
 	line = '=' * 65
 	content = '%s\n%-12s%-8s%-8s%-10s%-25s'%(line,'Date','Cost','Save','Balance','Comment')
 
@@ -107,12 +93,41 @@ def query_info():
 		for line in f:
 			print(line)
 
+# global date
+
+# def insert_date():
+
+#     # d = input("Select 1 to insert date or 2 to record today's date: ")
+#     # if d == 1:
+#     a = input("Please insert the date, yyyy-mm-dd: ")
+#     # This could be the drop down menu or just simply user input
+#     t = time.strptime(a, "%Y-%m-%d")
+#     y, m, d = t[0:3]
+#     date = datetime.datetime(y, m, d)
+#     # elif d == 2:
+#     #     date = time.strftime('%Y%m%d')
+#     # else:
+#     #     print("You can only select between 1 and 2! Please insert again!")
+#     #     sys.exit()
+#     return date
+
+
+
+
+
 def show_menu():
 	'''
 	Let user input their budget and total income
 	and if the balance is less than balance_check
 	show warning message
 	'''
+	# date = insert_date()
+	balance = 0.0
+	line = '=' * 65
+	content = '%-12s%-8s%-8s%-10s%-25s\n%s'%('Date','Cost','Save','Balance','Comment',line)
+	f = open('record.txt','w')
+	f.write(str(content))
+
 	budget = float(input("Input the number of maximum spending this month: "))
 	total_income = float(input('Input total income this month: '))
 	balance_check = total_income - budget
@@ -188,8 +203,25 @@ def stock_live():
 		plt.show()
 
 
-
+def init_view():
+	model.view_application = view.init_view()
+	#This connect to view.py to start the gui 
+	view.init_menu_screen()
+	#if we have a model.py
+# class Parameters:
+# 	view_application = ""
 
 if __name__ =='__main__':
-	# show_menu()
-	stock_live()
+	show_menu()
+	# spend_money()
+	# insert_date()
+	# init_view()# This is help with the 
+	# stock_live()
+
+
+
+
+
+
+
+#
